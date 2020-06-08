3cb3914615ee02a1b33dacd483d1a7a6e05025e6
im-ishaanm <mukherjeeishaan.2001@gmail.com>
2020-06-08 15:17:36 +0530

    added list of items on adming page

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,30 @@
 import React, { Component } from "react";
+import "../css/Item.css";
 
 class Item extends Component {
+  testClick = () => {
+    alert("Good");
+  };
+
   render() {
     const { item } = this.props;
+
+    let isImageUploaded = item.imageUploaded ? (
+      <p className="image-yes">Image Uploaded</p>
+    ) : (
+      <p className="image-no">No Image Found</p>
+    );
+
     return (
-      <div>
-        <p>{item.name}</p>
+      <div onClick={this.testClick} className="admin-item-wrapper">
+        <div className="content-one">
+          <h3>{item.name}</h3>
+          <p>{item.desc}</p>
+        </div>
+        <div className="content-two">
+          <h3 className="price-tag">Rs. {item.price}/-</h3>
+          <div className="image-upload-check">{isImageUploaded}</div>
+        </div>
       </div>
     );
   }
