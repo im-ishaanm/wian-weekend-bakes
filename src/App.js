0debfe5966f9db0e27bdfea2b8eef926f14594be
im-ishaanm <mukherjeeishaan.2001@gmail.com>
2020-06-10 19:08:04 +0530

    added material ui theme provider

--- src/App.js
+++ src/App.js
@@ -13,16 +13,41 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "./redux/store";
 
+// Material UI stuff
+import { ThemeProvider as MuiThemeProvider } from "@material-ui/core/styles";
+import createMuiTheme from "@material-ui/core/styles/createMuiTheme";
+
+const themeData = {
+  palette: {
+    primary: {
+      light: "#48cae4",
+      main: "#0096c7",
+      dark: "#023e8a",
+      contrastText: "#fff",
+    },
+    secondary: {
+      light: "#ff6b6b",
+      main: "#d00000",
+      dark: "#6a040f",
+      contrastText: "#fff",
+    },
+  },
+};
+
+const theme = createMuiTheme(themeData);
+
 function App() {
   return (
     <Provider store={store}>
-      <Router>
-        <div className="App">
-          <Nav />
-          <Route path="/admin" component={Admin} exact />
-          <Route path="/" component={Home} exact />
-        </div>
-      </Router>
+      <MuiThemeProvider theme={theme}>
+        <Router>
+          <div className="App">
+            <Nav />
+            <Route path="/admin" component={Admin} exact />
+            <Route path="/" component={Home} exact />
+          </div>
+        </Router>
+      </MuiThemeProvider>
     </Provider>
   );
 }
